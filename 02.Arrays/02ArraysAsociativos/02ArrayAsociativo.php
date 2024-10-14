10506e82c7a486b2eac135f90f5c92e403106281
cemeache <135959049+cemeache@users.noreply.github.com>
2024-10-14 18:28:24 +0200

    Entrega - Organizado

---
+++ 02.Arrays/02ArraysAsociativos/02ArrayAsociativo.php
@@ -0,0 +1,19 @@
+<?php
+    $enlacesImgs = array("margarita"=>"margarita.jpg",
+    "clavel"=>"clavel.jpg",
+    "petunia"=>"petunia.jpg");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Flores</title>
+        <link rel="stylesheet" href="./02style.css">
+    </head>
+    <body>
+        <?php
+            foreach ($enlacesImgs as $nombre => $enlace)
+                echo "<figure><img src='./02Img/".$enlace."'><figcaption>".$nombre."</figcaption></figure>";
+        ?>
+    </body>
+</html>
