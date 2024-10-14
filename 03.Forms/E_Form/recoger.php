a8eaff70ae7f11b62e0b109dac32e587dadbb91f
cemeache <135959049+cemeache@users.noreply.github.com>
2024-10-14 18:33:09 +0200

    Organizar Entrega Ejercicio Form

---
+++ 03.Forms/E_Form/recoger.php
@@ -0,0 +1,51 @@
+<?php
+
+    $nombre = $_GET["nombre"];
+
+    $password = $_GET["password"];
+    
+    if(isset($_GET["deportes"])) {   
+        for($i = 0; $i < count($_GET["deportes"]); $i++)
+            $deportes[] = $_GET["deportes"][$i];
+    }   
+
+    if(isset($_GET["curso"]))
+        $curso = $_GET["curso"];
+    
+    $observaciones = $_GET["observaciones"];
+    
+    $ciclo = $_GET["ciclo"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Recoger Form</title>
+    </head>
+    <body>
+        <?php
+            if(!empty($nombre))
+                echo "<p>Nombre : ".$nombre."</p>";
+
+            if(!empty($password))
+                echo "<p>Contraseña : ".$password."</p>";
+
+            if(isset($curso))
+                echo "<p>Curso : ".$curso."</p>";
+
+            if(!empty($observaciones))
+                echo "<p>Observaciones : ".$observaciones."</p>";
+
+            if(!empty($ciclo))
+                echo "<p>Ciclo : ".$ciclo."</p>";
+
+            if (isset($deportes)) {
+                for ($i = 0; $i < count($deportes); $i++) {
+                    echo "<p>Deporte $i : ".$deportes[$i]."</p>";
+                }
+            }
+        ?>
+    </body>
+</html>
---
+++ 03.Forms/E_Form/recogerPost.php
@@ -0,0 +1,51 @@
+<?php
+
+    $nombre = $_POST["nombre"];
+
+    $password = $_POST["password"];
+    
+    if(isset($_POST["deportes"])) {   
+        for($i = 0; $i < count($_POST["deportes"]); $i++)
+            $deportes[] = $_POST["deportes"][$i];
+    }   
+
+    if(isset($_POST["curso"]))
+        $curso = $_POST["curso"];
+    
+    $observaciones = $_POST["observaciones"];
+    
+    $ciclo = $_POST["ciclo"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Recoger Form</title>
+    </head>
+    <body>
+        <?php
+            if(!empty($nombre))
+                echo "<p>Nombre : ".$nombre."</p>";
+
+            if(!empty($password))
+                echo "<p>Contraseña : ".$password."</p>";
+
+            if(isset($curso))
+                echo "<p>Curso : ".$curso."</p>";
+
+            if(!empty($observaciones))
+                echo "<p>Observaciones : ".$observaciones."</p>";
+
+            if(!empty($ciclo))
+                echo "<p>Ciclo : ".$ciclo."</p>";
+
+            if (isset($deportes)) {
+                for ($i = 0; $i < count($deportes); $i++) {
+                    echo "<p>Deporte $i : ".$deportes[$i]."</p>";
+                }
+            }
+        ?>
+    </body>
+</html>
