d93b66697e66606c35c10485545e0f1ed1fb464e
CeliaEVG <2DAW01@esvirgua.es>
2024-10-23 13:54:40 +0200

    05BBDD || Pruebasv2

---
+++ 05.BaseDatos/000Pruebas/nombreOpera/config.php
@@ -0,0 +1,6 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $contraseña = "";
+    $basedatos = "bdalum";
+?>
--- 05.BaseDatos/000Pruebas/nombreOpera/mySqlI.php
+++ 05.BaseDatos/000Pruebas/nombreOpera/mySqlI.php
@@ -5,21 +5,13 @@
     $mysqli = new mysqli($servidor, $usuario, $contraseña, $basedatos);
 
     /*Realizar una consulta a la base de datos*/
-    $consulta = "SELECT nombre,email FROM alumnos";
+    $consulta = "SELECT count(*) FROM alumnos";
 	$resultado = $mysqli->query($consulta);
 
     /*Extrae una fila del resultado de la consulta (método del objeto mysqli_result)*/
-	$fila = $resultado ->fetch_array(); 
+	$fila = $resultado ->fetch_assoc(); 
     
-    while($fila = $resultado ->fetch_array()){
-        foreach($fila as $indice => $valor){
-            echo $indice;
-            echo " -- ";
-            echo $valor;
-            echo "<br>";
-        }
-        echo "<hr>";
-    }
+    print_r($fila);
     
 
     /*Cerrar la conexión establecida*/
