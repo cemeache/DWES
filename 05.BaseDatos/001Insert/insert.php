375827210528378df3c29022b3ff016c907e4e7e
Merroji <156344132+merroji@users.noreply.github.com>
2024-10-24 21:11:06 +0200

    05BBDD || 001Insert || TryCatch CSU

--- 05.BaseDatos/001Insert/insert.php
+++ 05.BaseDatos/001Insert/insert.php
@@ -1,15 +1,19 @@
 <?php
     require_once("./conexion.php");
 
-    $consulta = "INSERT INTO alumnos (nombre,apellidos,email,telefono) VALUES ('".$_POST["nombre"]."','".$_POST["apellidos"]."','".$_POST["email"]."','".$_POST["telefono"]."');";
+    try{
+        $consulta = "INSERT INTO alumnos (nombre,apellidos,email,telefono) VALUES ('".$_POST["nombre"]."','".$_POST["apellidos"]."','".$_POST["email"]."','".$_POST["telefono"]."');";
 
-    /*echo $consulta;*/
+        /*echo $consulta;*/
 
-    if ($mysqli->query($consulta)) {
-        echo "Registro realizado correctamente";
-    } else {
-        echo "Error al registrar: " . $mysqli->error;
+        if($mysqli->query($consulta))
+            echo "Registro realizado correctamente";
+        else
+            throw new Exception("Error al registrar: " . $msqli->error);
+        
+        $mysqli->close();
+
+    }catch(Exception $e){
+        echo $e->getMessage();
     }
-
-    $mysqli->close();
 ?>
