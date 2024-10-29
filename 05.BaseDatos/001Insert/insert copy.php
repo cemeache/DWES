ffbe713f35038e9aa2765b78088114baae1aa30a
celia.evg <2DAW01@esvirgua.es>
2024-10-29 10:27:07 +0100

    Cambios BBDD

--- 05.BaseDatos/001Insert/insert copy.php
+++ 05.BaseDatos/001Insert/insert copy.php
@@ -1,21 +1,24 @@
 <?php
     require_once("./conexion.php");
 
-    try{
+    
         $consulta = "INSERT INTO alumnos (nombre,apellidos,email,telefono) VALUES ('".$_POST["nombre"]."','".$_POST["apellidos"]."','".$_POST["email"]."','".$_POST["telefono"]."');";
 
         /*Desactivar notificacion de error*/
         $controlador = new mysqli_driver();
         $controlador->report_mode = MYSQLI_REPORT_OFF;
 
+        /*Comprobar el error de la última conexión
+	    $mysqli->connect_errno; //num error
+        $mysqli->connect_error; //texto error*/
+
+        /*echo $consulta;*/
+
         if($mysqli->query($consulta))
             echo "Registro realizado correctamente";
         else
-            throw new Exception("Error al registrar: " . $mysqli->error .' '.$mysqli->errno);
-        
+            echo "Fallo en el registro";
+       
         $mysqli->close();
-
-    }catch(Exception $e){
-        echo $e->getMessage();
-    }
+    
 ?>
