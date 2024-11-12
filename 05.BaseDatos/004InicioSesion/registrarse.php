f49249e4b0cd842a289e8052532a8750bf140250
celia.evg <2DAW01@esvirgua.es>
2024-11-12 10:34:56 +0100

    05BBDD | Registro Realizado

--- 05.BaseDatos/004InicioSesion/registrarse.php
+++ 05.BaseDatos/004InicioSesion/registrarse.php
@@ -9,30 +9,37 @@
     $password = $_POST["password"];
     $passwordV2 = $_POST["passwordV2"];
 
-    // Verificar que las contraseñas coincidan
-    if ($password !== $passwordV2) {
-        $mnsj = "ERROR | Las contraseñas no coinciden.";
+    try {
+        if ($password != $passwordV2) {
+            throw new Exception("ERROR | Las contraseñas no coinciden");
+            
+        }
+        // Verificar que las contraseñas coincidan
+        $consulta = "INSERT INTO usuario (nombre, apellidos, nombreUsu, email, contrasena) VALUES ('".$nombre."', '".$apellidos."', '".$nombreUsu."', '".$email."', '".$password."')";
+    
+        try {
+            // Ejecutar la consulta
+            if ($mysqli->query($consulta))
+                $mnsj = "Registro realizado correctamente.";
+            else
+                if ($mysqli->errno == 1062)
+                    $mnsj = "ERROR | El nombre de usuario o el correo electrónico ya están registrados.";
+                else
+                    $mnsj = "ERROR | Ha ocurrido un error, no se pudo completar el registro";
+        } catch (Exception $e) {
+            $mnsj = $e->getMessage();
+        }   
+        
+        // Incluir el formulario para mostrar el mensaje
+        require_once("./form.php"); 
+    
+    } catch (Exception $e) {
+        $mnsj = $e->getMessage();
         require_once("./formRgt.php");
     }
+    
+    // Cerrar la conexión
+    $mysqli->close();
 
-    $consulta = "INSERT INTO usuario (nombre, apellidos, nombreUsu, email, contrasena) VALUES ('".$nombre."', '".$apellidos."', '".$nombreUsu."', '".$email."', '".$password."')";
-
-    try {
-    // Ejecutar la consulta
-    if ($mysqli->query($consulta))
-        $mnsj = "Registro realizado correctamente.";
-    else
-        if ($mysqli->errno == 1062)
-            $mnsj = "ERROR | El nombre de usuario o el correo electrónico ya están registrados.";
-        else
-            $mnsj = "ERROR | Ha ocurrido un error, no se pudo completar el registro";
-} catch (Exception $e) {
-    $mnsj = $e->getMessage();
-}
-
-// Cerrar la conexión
-$mysqli->close();
-
-// Incluir el formulario para mostrar el mensaje
-require_once("./form.php");
+    
 ?>
