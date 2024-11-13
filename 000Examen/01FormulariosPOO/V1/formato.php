e352f031e5cfd7b5db948a85bb91dc8cb64f68b6
Merroji <156344132+merroji@users.noreply.github.com>
2024-11-13 19:14:13 +0100

    Corrección

--- 000Examen/01FormulariosPOO/V1/formato.php
+++ 000Examen/01FormulariosPOO/V1/formato.php
@@ -1,15 +1,19 @@
 <?php
 	class Formato{
-		function numCaracteres(string $cadena){
-			return strlen($cadena);
-		}
+		public $longitud;
 		
 		function apellidoNombre(string $apellido = "", string $nombre = ""){
+			$this->longitud = $this->numCaracteres($nombre.$apellido);
 			return $apellido.', '.$nombre;
 		}
 		
 		function nombreApellido(string $apellido = "", string $nombre = ""){
+			$this->longitud = $this->numCaracteres($nombre.$apellido);
 			return $nombre.' '.$apellido;
+		}
+		
+		private function numCaracteres(string $cadena){
+			return strlen($cadena);
 		}
 	}
 ?>
--- 000Examen/01FormulariosPOO/V1/main.php
+++ 000Examen/01FormulariosPOO/V1/main.php
@@ -1,37 +1,26 @@
 <?php
 	/*Incluimos la clase*/
 	require_once("formato.php");
-	
-	/*Comprobar si los campos recogidos del form están creados o rellenos*/
-	if(!empty($_POST["name"]))
-		$nombre = $_POST["name"];
-	
-	if(!empty($_POST["apellido"]))
-		$apellido = $_POST["apellido"];
-	
-	if(isset($_POST["provincia"]))
-		$provincia = $_POST["provincia"];
-	
-	if(isset($_POST["formato"]))
-		$formato = $_POST["formato"];
-	
+
 	/*Instanciamos el objeto*/
 	$objFormato = new Formato();
 	
 	/*Si introducen al menos uno de los campos(apellido / nombre) y existe $formato ejecuta los métodos*/
-	if(isset($apellido) && isset($nombre) && isset($formato)){
+	if(isset($_POST["apellido"]) && isset($_POST["name"]) && isset($_POST["formato"])){
 		if($formato == "Apellido, Nombre")
-		$cadena = $objFormato->apellidoNombre($apellido,$nombre);
-	else
-		$cadena = $objFormato->nombreApellido($apellido,$nombre);
+			$cadena = $objFormato->apellidoNombre($apellido,$nombre);
+		else
+			$cadena = $objFormato->nombreApellido($apellido,$nombre);
 	
-	echo "<p>Nombre Completo: ".$cadena."</p>";
-	echo "<p>Longitud: ".$objFormato->numCaracteres($cadena)."</p>";
+		echo "<p>Nombre Completo: ".$cadena."</p>";
+		echo "<p>Longitud: ".$objFormato->longitud."</p>";
 	}else
 		echo "<p>Formulario Incompleto</p>";
 	
 	/*Comprobamos que existe $provincia*/
-	if(isset($provincia))
+	if(isset($_POST["provincia"]))
 		echo "<p>Provincia: ".$provincia."</p>";
+	else
+		echo "<p>Provincia no seleccionada</p>";
 	
 ?>
